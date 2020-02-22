eb8269d5d6e1c509deffc2929947a6af9b109c46
Streeter <armoredsky@gmail.com>
2020-02-22 17:55:22 -0600

    MS: added html parser and moved api code to it's own dir

---
+++ server/api/hello_world.js
@@ -0,0 +1,23 @@
+const html_scrapper = require('../lib/html_scrapper')
+
+const initialize = (app) => {
+  app.get('/api/hello', (req, res) => {
+    res.send({ express: 'Hello From Express' });
+  });
+
+  app.post('/api/world', async (req, res) => {
+    try {
+      console.log(req.body);
+
+      const body = await html_scrapper.logHtml('https://news.ycombinator.com')
+      console.log(body)
+
+      res.send(`I received your POST request. This is what you sent me: ${req.body.post}`);
+    } catch (error) {
+      res.send("shit went down");
+    }
+  });
+}
+module.exports = {
+  initialize
+}
---
+++ server/lib/html_scrapper.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch')
+
+const logHtml = async (url) => {
+  try {
+    const res = await   fetch(url)
+    const body = await res.text()
+    return body
+
+  } catch(error) {
+        console.log("something went wrong scrapping url", url, error)
+  }
+}
+
+module.exports = {
+  logHtml
+}
