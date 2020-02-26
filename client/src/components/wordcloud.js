c10622c26b7d2fe87b4b46c7a7ae3cbcd54559e7
Streeter <armoredsky@gmail.com>
2020-02-26 15:12:59 -0600

    MS: updatd to type in text and build wordcloud

---
+++ client/src/components/wordcloud.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import Wordcloud from 'react-wordcloud'
+
+export const Cloud = ({words}) => {
+  return <Wordcloud words={words}/>
+}
+export default Cloud
--- server/lib/html_scrapper.js
+++ server/lib/html_scrapper.js
@@ -11,6 +11,7 @@ const logHtml = async (url) => {
   }
 }
 
+
 module.exports = {
   logHtml
 }
